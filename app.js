09c498f6ec3b08eb3540498eaa49901e74cc3efa
lxzhu <lxzhu@outlook.com>
2014-04-16 14:47:45 +0800

    we're using express 4.0 now. almost all middleware are removed from express and become independent module, I have to add them back one by one.

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 var express = require('express');
-
+var redirect =require('express-redirect');
 var http = require('http');
 var path = require('path');
 var app = express();
+redirect(app);
 
-app.configure(function() {
 	app.set('port', process.env.PORT || 8000);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
-	app.use(express.favicon());
+	//favicon is removed. could use https://www.npmjs.org/package/static-favicon
+	//app.use(express.favicon()); 
 	//TODO: how to config so it log to file?
-	app.use(express.logger('dev'));
-	app.use(express.json());
+	//app.use(express.logger('dev'));
+	//app.use(express.json());
 	//TODO: how to config it to allow url rewrite?
-	app.use(express.urlencoded());
-	app.use(express.query());
-	app.use(express.cookieParser());
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(express.static(path.join(__dirname, 'pubs')));
-});
-app.configure('development', function() {
-	app.use(express.errorHandler());
-});
+	//app.use(express.urlencoded());
+	//app.use(express.query());
+	//app.use(express.cookieParser());
+	//app.use(express.bodyParser());
+	//app.use(express.methodOverride());
+	//app.use(app.router);
+	app.use('/pubs',express.static(path.join(__dirname, 'pubs')));
 
 
+	//app.use(express.errorHandler());
+
+app.redirect('/','/pubs/index.htm');
+/*
+app.get('/',function(req,res){
+  res.writeHead(200,{'Content-Type':'text/html'});
+  res.end("<h1>Hello World</h1>");
+});
+*/
 app.get('/view/:view', function(req, res) {
 	res.render(req.params.view, {
 		title : 'zhu liangxiong'

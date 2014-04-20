32734ebc420d15f7986f285eef804c98cffe650e
lxzhu <lxzhu@outlook.com>
2014-04-20 11:15:09 +0800

    add sample api file and rename data_access files

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
 var express = require('express');
-var redirect =require('express-redirect');
+var bodyParser=require('body-parser');
+var cookieParser=require('cookie-parser');
+var redirect = require('express-redirect');
 var http = require('http');
 var path = require('path');
 var app = express();
 redirect(app);
 
-	app.set('port', process.env.PORT || 80);
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	//favicon is removed. could use https://www.npmjs.org/package/static-favicon
-	//app.use(express.favicon()); 
-	//TODO: how to config so it log to file?
-	//app.use(express.logger('dev'));
-	//app.use(express.json());
-	//TODO: how to config it to allow url rewrite?
-	//app.use(express.urlencoded());
-	//app.use(express.query());
-	//app.use(express.cookieParser());
-	//app.use(express.bodyParser());
-	//app.use(express.methodOverride());
-	//app.use(app.router);
-	app.use('/pubs',express.static(path.join(__dirname, 'pubs')));
+app.set('port', process.env.PORT || 80);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+//favicon is removed. could use https://www.npmjs.org/package/static-favicon
+//app.use(express.favicon()); 
+//TODO: how to config so it log to file?
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//TODO: how to config it to allow url rewrite?
+//app.use(express.urlencoded());
+//app.use(express.query());
+app.use(express.cookieParser);
+app.use(bodyParser);
+//app.use(express.methodOverride());
+//app.use(app.router);
+app.use('/pubs', express.static(path.join(__dirname, 'pubs')));
 
 
-	//app.use(express.errorHandler());
+//app.use(express.errorHandler());
 
-app.redirect('/','/pubs/index.htm');
+app.redirect('/', '/pubs/index.htm');
 /*
 app.get('/',function(req,res){
   res.writeHead(200,{'Content-Type':'text/html'});
   res.end("<h1>Hello World</h1>");
 });
 */
-app.get('/view/:view', function(req, res) {
+app.get('/views/:view', function(req, res) {
 	res.render(req.params.view, {
-		title : 'zhu liangxiong'
+		title: 'zhu liangxiong'
 	});
 });
+
+app.post('/api/proto_product_code/add',
 
 http.createServer(app).listen(app.get('port'), function(error) {
 	if (error) {
